139b035b5a815cb10c975119c9e73ad40d22c7c8
KCRebels <myeldiablo@yahoo.com>
2026-09-19 14:39:10 -0500

    Publish coach portal reconnect recovery

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const BUILD_VERSION = '2026.09.19.29';
+const BUILD_VERSION = '2026.09.19.30';
 const CACHE_PREFIX = 'hotb-app-';
 const CACHE_NAME = `${CACHE_PREFIX}${BUILD_VERSION}`;
 const OFFLINE_SHELL = './index.html';
