d6add0115e4e807a8251180152ed7860a07e5d39
KCRebels <myeldiablo@yahoo.com>
2026-09-19 17:45:44 -0500

    Publish service-worker.js build 116

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const BUILD_VERSION = '2026.09.19.115';
+const BUILD_VERSION = '2026.09.19.116';
 const CACHE_PREFIX = 'hotb-app-';
 const CACHE_NAME = `${CACHE_PREFIX}${BUILD_VERSION}`;
 const OFFLINE_SHELL = './index.html';
