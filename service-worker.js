4a3e1979beb67832eceb4614962a36e7e4a2c2da
KCRebels <myeldiablo@yahoo.com>
2026-09-19 14:28:17 -0500

    Restore coach app shell after portal diagnostic regression

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const BUILD_VERSION = '2026.09.19.26';
+const BUILD_VERSION = '2026.09.19.27';
 const CACHE_PREFIX = 'hotb-app-';
 const CACHE_NAME = `${CACHE_PREFIX}${BUILD_VERSION}`;
 const OFFLINE_SHELL = './index.html';
