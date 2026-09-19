8aa5cef834e8b54b2d492c32da267aa7c5497b86
KCRebels <myeldiablo@yahoo.com>
2026-09-19 16:42:22 -0500

    Publish portal refresh fix build

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const BUILD_VERSION = '2026.09.19.75';
+const BUILD_VERSION = '2026.09.19.76';
 const CACHE_PREFIX = 'hotb-app-';
 const CACHE_NAME = `${CACHE_PREFIX}${BUILD_VERSION}`;
 const OFFLINE_SHELL = './index.html';
