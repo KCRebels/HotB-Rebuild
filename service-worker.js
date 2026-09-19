8ba09902a84db75366112d7fbdcfda4acdfb62da
KCRebels <myeldiablo@yahoo.com>
2026-09-19 17:03:18 -0500

    Advance service-worker.js for portal auth claim hardening

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const BUILD_VERSION = '2026.09.19.85';
+const BUILD_VERSION = '2026.09.19.86';
 const CACHE_PREFIX = 'hotb-app-';
 const CACHE_NAME = `${CACHE_PREFIX}${BUILD_VERSION}`;
 const OFFLINE_SHELL = './index.html';
