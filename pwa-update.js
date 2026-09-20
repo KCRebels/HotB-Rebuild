51842f928c7930ec57f344c1575d553c405956a7
KCRebels <myeldiablo@yahoo.com>
2026-09-19 20:19:58 -0500

    Publish pwa-update.js build 193

--- pwa-update.js
+++ pwa-update.js
@@ -1,5 +1,5 @@
 (() => {
-  const BUILD_VERSION = '2026.09.19.192';
+  const BUILD_VERSION = '2026.09.19.193';
   window.HOTB_BUILD_VERSION = BUILD_VERSION;
   // Player portals must always use the current network app. They do not install,
   // update, or re-register the coach PWA service worker.
