8aa3d78934dc5bc0a60bc6b02cedf8737b46b3e9
KCRebels <myeldiablo@yahoo.com>
2026-09-19 20:32:25 -0500

    Publish service-worker.js build 197

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,8 @@
-const BUILD_VERSION = '2026.09.19.196';
+const BUILD_VERSION = '2026.09.19.197';
 const CACHE_PREFIX = 'hotb-app-';
 const CACHE_NAME = `${CACHE_PREFIX}${BUILD_VERSION}`;
 const OFFLINE_SHELL = './index.html';
-const CANONICAL_LAUNCH = './?source=pwa&launch=196';
+const CANONICAL_LAUNCH = './?source=pwa&launch=197';
 const LEGACY_SHELL = './hotb-fresh.html';
 const CORE_FILES = ['./index.html', './hotb-fresh.html', './styles.css', './evaluation-cleanup.css', './app.js'];
 const VERSIONED_CORE_PATTERNS = [/\/app\.js(?:\?|$)/, /\/styles\.css(?:\?|$)/, /\/evaluation-cleanup\.css(?:\?|$)/];
