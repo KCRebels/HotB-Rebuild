4838f7932b05feb63951f0a2d3e4af93ca794c3f
KCRebels <myeldiablo@yahoo.com>
2026-09-22 06:51:41 -0500

    Advance installed-app updater to build 230

--- pwa-update.js
+++ pwa-update.js
@@ -1,5 +1,5 @@
 (() => {
-  const BUILD_VERSION = '2026.09.22.229';
+  const BUILD_VERSION = '2026.09.22.230';
   window.HOTB_BUILD_VERSION = BUILD_VERSION;
   // Player portals must always use the current network app. They do not install,
   // update, or re-register the coach PWA service worker.
@@ -31,7 +31,7 @@
       }catch(_){}
       const url=new URL('./',window.location.href);
       url.searchParams.set('source','pwa');
-      url.searchParams.set('launch','229');
+      url.searchParams.set('launch','230');
       url.searchParams.set('hotb-update',version);
       url.searchParams.set('reload',Date.now().toString());
       window.location.replace(url.href);
