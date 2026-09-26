8106666bc899d8a7161761682cd62b850912ee66
KCRebels <myeldiablo@yahoo.com>
2026-09-26 13:15:22 -0500

    Step 2 use uploaded HotB logo asset

--- app.js
+++ app.js
@@ -2240,12 +2240,7 @@ function homeView(){
  return `<div class="home-hero">
    <div class="home-brand">
     <img class="home-logo-img" src="Rebels%20REG%20White%20with%20red%20wing%20-%20REGIONAL.png" alt="Kansas City Rebels Regional">
-    <div class="home-hotb-mark" aria-label="Rebels HotB Elite Hitting App">
-      <div class="home-hotb-rebels">REBELS</div>
-      <div class="home-hotb-name">HotB</div>
-      <div class="home-hotb-bat"></div>
-      <div class="home-hotb-tag">Elite Hitting App</div>
-    </div>
+    <img class="home-hotb-logo-asset" src="IMG_1666.jpg" alt="Rebels HotB Elite Hitting App">
    </div>
    <div class="home-actions">
     <button class="home-card primary" data-go="new"><h3>New Game</h3></button>
